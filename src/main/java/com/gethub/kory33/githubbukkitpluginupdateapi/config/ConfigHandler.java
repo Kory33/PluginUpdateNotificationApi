d8039f28de660cde0757eb8fc659bca1397d444b
Kory33 <konryoitdv@gmail.com>
2016-12-09 21:57:55 +0530

    added implementation for shouldLogUpToDate

--- src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
+++ src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
@@ -13,11 +13,21 @@ import com.gethub.kory33.githubbukkitpluginupdateapi.UpdateNotifyPlugin;
  *
  */
 public class ConfigHandler {
+    private static final String LOG_UP_TO_DATE_PATH = "logUpToDate";
+    
     /** Path to the configuration file */
     private final FileConfiguration fConfiguration;
 
     public ConfigHandler(UpdateNotifyPlugin plugin, String configPath){
         File file = new File(plugin.getDataFolder(), configPath);
         this.fConfiguration = YamlConfiguration.loadConfiguration(file);
+    }
+    
+    /**
+     * Return true, in reference to the configuration file, if the plugin should log up-to-date status
+     * @return value of LOG_UP_TO_DATE_PATH defined in ConfigHandler
+     */
+    public boolean shouldLogUpToDate(){
+        return this.fConfiguration.getBoolean(LOG_UP_TO_DATE_PATH, false);
     }
 }
