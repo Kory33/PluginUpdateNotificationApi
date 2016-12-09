248e4cd6e3641e0b86ea217d19d3c11519de5fc5
Kory33 <konryoitdv@gmail.com>
2016-12-09 21:37:15 +0530

    added config handler

---
+++ src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
@@ -0,0 +1,15 @@
+package com.gethub.kory33.githubbukkitpluginupdateapi.config;
+
+/**
+ * Bukkit-configuration handler
+ * @author Kory33
+ *
+ */
+public class ConfigHandler {
+    /** Path to the configuration file */
+    private final String configPath;
+
+    public ConfigHandler(String configPath){
+        this.configPath = configPath;
+    }
+}
