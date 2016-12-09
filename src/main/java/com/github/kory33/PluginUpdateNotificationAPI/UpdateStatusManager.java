f1405506cca8f9abe4c9b037df5ee493a9915e1f
Kory33 <konryoitdv@gmail.com>
2016-12-10 00:05:14 +0530

    added github update checker class

---
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/UpdateStatusManager.java
@@ -0,0 +1,5 @@
+package com.github.kory33.PluginUpdateNotificationAPI;
+
+public class UpdateStatusManager {
+
+}
---
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/github/GithubUpdateNotifyPlugin.java
@@ -0,0 +1,36 @@
+package com.github.kory33.PluginUpdateNotificationAPI.github;
+
+import com.github.kory33.PluginUpdateNotificationAPI.UpdateNotifyPlugin;
+
+public abstract class GithubUpdateNotifyPlugin extends UpdateNotifyPlugin {
+
+    @Override
+    public boolean checkForUpdate() {
+        // TODO 自動生成されたメソッド・スタブ
+        return false;
+    }
+
+    @Override
+    public String getUpdateLogString() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    @Override
+    public String getUpdatePlayerLogString() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    @Override
+    public String getUpToDateLogString() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    @Override
+    public String getUpToDatePlayerLogString() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+}
