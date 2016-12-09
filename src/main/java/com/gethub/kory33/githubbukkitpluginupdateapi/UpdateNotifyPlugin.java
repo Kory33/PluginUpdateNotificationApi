317422ef0233824749be8c69288d8eb218c6ab8a
Kory33 <konryoitdv@gmail.com>
2016-12-09 20:56:13 +0530

    added members to the core class

--- src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/UpdateNotifyPlugin.java
+++ src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/UpdateNotifyPlugin.java
@@ -3,15 +3,27 @@ package com.gethub.kory33.githubbukkitpluginupdateapi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class UpdateNotifyPlugin extends JavaPlugin {
+    /**
+     * Returns true if the plugin's newer version is released
+     * @return true if the plugin's newer version is released
+     */
+    public abstract boolean getUpdateStatus();
+    
     @Override
     public void onEnable() {
-        // TODO 自動生成されたメソッド・スタブ
+        super.getServer().getLogger().info("Embedded UpdateNotifyPlugin is enabled for " + getPluginName());
         super.onEnable();
     }
     
     @Override
     public void onDisable() {
-        // TODO 自動生成されたメソッド・スタブ
+        super.getServer().getLogger().info("Disabled UpdateNotifyPlugin is disabled for " + getPluginName());
         super.onDisable();
     }
+    
+    /**
+     * Returns the plugin name that will be displayed to the log
+     * @return plugin name
+     */
+    public abstract String getPluginName();
 }
