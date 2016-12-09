823c87ce15c1ee8bcbb96f172eeeba7daba1453d
Kory33 <konryoitdv@gmail.com>
2016-12-09 22:11:46 +0530

    added implementation for isUpdateCheckFrequent

--- src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
+++ src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
@@ -14,10 +14,12 @@ import com.gethub.kory33.githubbukkitpluginupdateapi.UpdateNotifyPlugin;
  */
 public class ConfigHandler {
     private static final String LOG_UP_TO_DATE_PATH = "logUpToDate";
+    private static final String UPDATE_CHECK_FREQUENT_PATH = "updateCheckFrequent";
     
     /** Path to the configuration file */
     private final FileConfiguration fConfiguration;
-
+    
+    
     public ConfigHandler(UpdateNotifyPlugin plugin, String configPath){
         File file = new File(plugin.getDataFolder(), configPath);
         this.fConfiguration = YamlConfiguration.loadConfiguration(file);
@@ -25,9 +27,18 @@ public class ConfigHandler {
     
     /**
      * Return true, in reference to the configuration file, if the plugin should log up-to-date status
-     * @return value of LOG_UP_TO_DATE_PATH defined in ConfigHandler
+     * @return value of {@value #LOG_UP_TO_DATE_PATH} in the config file
      */
     public boolean shouldLogUpToDate(){
         return this.fConfiguration.getBoolean(LOG_UP_TO_DATE_PATH, false);
+    }
+    
+    /**
+     * Return true, in reference to the configuration file, if the plugin should check for update frequently.
+     * @return value of {@value #UPDATE_CHECK_FREQUENT_PATH} in the config file
+     */
+    
+    public boolean isUpdateCheckFrequent(){
+        return this.fConfiguration.getBoolean(UPDATE_CHECK_FREQUENT_PATH, false);
     }
 }
