e7900e8152eb7de3de60dcc723aab8de30bef152
Kory33 <konryoitdv@gmail.com>
2016-12-09 21:50:02 +0530

    modified member/constructor of ConfigHandler

--- src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
+++ src/main/java/com/gethub/kory33/githubbukkitpluginupdateapi/config/ConfigHandler.java
@@ -1,5 +1,12 @@
 package com.gethub.kory33.githubbukkitpluginupdateapi.config;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.gethub.kory33.githubbukkitpluginupdateapi.UpdateNotifyPlugin;
+
 /**
  * Bukkit-configuration handler
  * @author Kory33
@@ -7,9 +14,10 @@ package com.gethub.kory33.githubbukkitpluginupdateapi.config;
  */
 public class ConfigHandler {
     /** Path to the configuration file */
-    private final String configPath;
+    private final FileConfiguration fConfiguration;
 
-    public ConfigHandler(String configPath){
-        this.configPath = configPath;
+    public ConfigHandler(UpdateNotifyPlugin plugin, String configPath){
+        File file = new File(plugin.getDataFolder(), configPath);
+        this.fConfiguration = YamlConfiguration.loadConfiguration(file);
     }
 }
