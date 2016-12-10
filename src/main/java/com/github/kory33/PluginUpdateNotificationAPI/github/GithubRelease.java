09a45d0827ca923bee537a23e5e5ca831a2c8290
Kory33 <konryoitdv@gmail.com>
2016-12-10 06:58:32 +0100

    added GithubRelease/GithubVersionManager classes

---
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/github/GithubRelease.java
@@ -0,0 +1,15 @@
+package com.github.kory33.PluginUpdateNotificationAPI.github;
+
+public class GithubRelease {
+
+    public String getVersion() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    public String getLink() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+}
---
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/github/GithubVersionManager.java
@@ -0,0 +1,26 @@
+package com.github.kory33.PluginUpdateNotificationAPI.github;
+
+import java.util.List;
+
+public class GithubVersionManager {
+
+    public List<GithubRelease> getVersionReleases() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    public List<GithubRelease> getReleases() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    public GithubRelease getLatestVersionRelease() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+    public GithubRelease getLatestRelease() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+}
