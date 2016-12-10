6ae9725aef1f73a53274ec3e51752661602498b9
Kory33 <konryoitdv@gmail.com>
2016-12-10 06:18:32 +0100

    added followVersion member

--- src/main/java/com/github/kory33/PluginUpdateNotificationAPI/github/GithubUpdateNotifyPlugin.java
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/github/GithubUpdateNotifyPlugin.java
@@ -3,34 +3,43 @@ package com.github.kory33.PluginUpdateNotificationAPI.github;
 import com.github.kory33.PluginUpdateNotificationAPI.UpdateNotifyPlugin;
 
 public abstract class GithubUpdateNotifyPlugin extends UpdateNotifyPlugin {
+    /**
+     * <p>
+     * This function is expected to return true if the plugin should follow <strong>only version releases.</strong>
+     * Version releases are the releases which are tagged with version, for instance, 1.0.2 or 0.5.3.2.
+     * <p>
+     * For the release to be considered as "new version", there should be increase in the version number,
+     * and the release must have a tag that matches the following regular expression: ($v?%d(\.%d)*)
+     */
+    public abstract boolean followVersions();
 
     @Override
     public boolean checkForUpdate() {
-        // TODO 自動生成されたメソッド・スタブ
+        // TODO implementation
         return false;
     }
 
     @Override
     public String getUpdateLogString() {
-        // TODO 自動生成されたメソッド・スタブ
+        // TODO implementation
         return null;
     }
 
     @Override
     public String getUpdatePlayerLogString() {
-        // TODO 自動生成されたメソッド・スタブ
+        // TODO implementation
         return null;
     }
 
     @Override
     public String getUpToDateLogString() {
-        // TODO 自動生成されたメソッド・スタブ
+        // TODO implementation
         return null;
     }
 
     @Override
     public String getUpToDatePlayerLogString() {
-        // TODO 自動生成されたメソッド・スタブ
+        // TODO implementation
         return null;
     }
 }
