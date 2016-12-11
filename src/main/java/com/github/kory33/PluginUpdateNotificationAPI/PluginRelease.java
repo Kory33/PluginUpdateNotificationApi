9b42a9f728c20efe51e24bb2db43427ef90d6b1f
Kory33 <konryoitdv@gmail.com>
2016-12-11 01:05:20 +0100

    partial implementation of PluginRelease

--- src/main/java/com/github/kory33/PluginUpdateNotificationAPI/PluginRelease.java
+++ src/main/java/com/github/kory33/PluginUpdateNotificationAPI/PluginRelease.java
@@ -1,15 +1,20 @@
 package com.github.kory33.PluginUpdateNotificationAPI;
 
 public abstract class PluginRelease {
-
+    private final String version;
+    private final String linkURL;
+    
+    public PluginRelease(String version, String linkURL){
+        this.version = version;
+        this.linkURL = linkURL;
+    }
+    
     public String getVersion() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return version;
     }
 
     public String getLink() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return linkURL;
     }
 
     public abstract boolean isNewerThanCurrent();
