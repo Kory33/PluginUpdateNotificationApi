3dfc2448a8da6d22ea2414a042797e83a7e0369f
Kory33 <konryoitdv@gmail.com>
2016-12-12 11:18:45 +0100

    added methods to SemanticPluginVersioning

--- src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
+++ src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
@@ -1,13 +1,50 @@
 package com.github.kory33.UpdateNotificationPlugin.versioning;
 
+/**
+ * Class that represents the semantic versioning of the plugin.
+ * <p>
+ * The concept of semantic versioning comforms to the document in
+ * <a href="http://semver.org/">http://semver.org/</a>
+ * @author Kory33, Bnana877
+ *
+ */
 public class SemanticPluginVersion extends PluginVersion{
-    //TODO implementation
+    /** major version of the plugin */
+    private int major;
+    
+    /** minor version of the plugin */
+    private int minor;
+    
+    /** patch version of the plugin */
+    private int patch;
+    
+    /** identifier of the plugin verison */
+    private String identifier;
+
+    /** build metadata of the plugin verison */
+    private String buildMetadata;
     
     public SemanticPluginVersion(String versionString) {
         super(versionString);
         //TODO implementation
     }
-
+    
+    /**
+     * Returns true if the version contains any identifier.
+     * @return
+     */
+    public boolean hasIdentifier(){
+        return !this.identifier.equals("");
+    }
+    
+    /**
+     * Returns true if the version contains any metadata
+     * @return true if the version contains any metadata
+     */
+    public boolean hasBuildMetadata(){
+        return !this.buildMetadata.equals("");
+    }
+    
     @Override
     public boolean isNewerThan(String versionString) {
         //TODO implementation
