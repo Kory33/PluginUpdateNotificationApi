1a4f99d1f78d02fea0a2457cea0b51e2caa527c0
Kory33 <konryoitdv@gmail.com>
2016-12-12 11:00:20 +0100

    added SemanticPluginVersion class

---
+++ src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
@@ -0,0 +1,16 @@
+package com.github.kory33.UpdateNotificationPlugin.versioning;
+
+public class SemanticPluginVersion extends PluginVersion{
+    //TODO implementation
+    
+    public SemanticPluginVersion(String versionString) {
+        super(versionString);
+        //TODO implementation
+    }
+
+    @Override
+    public boolean isNewerThan(String versionString) {
+        //TODO implementation
+        return false;
+    }
+}
