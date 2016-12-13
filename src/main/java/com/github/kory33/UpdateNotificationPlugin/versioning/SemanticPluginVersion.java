4aaa768e39ae97453f27ef48a99e9264be367352
Bnana877 <kikuryo1209@gmial.com>
2016-12-13 16:45:27 +0900

    test

--- src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
+++ src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
@@ -1,5 +1,4 @@
 package com.github.kory33.UpdateNotificationPlugin.versioning;
-
 /**
  * Class that represents the semantic versioning of the plugin.
  * <p>
@@ -26,7 +25,30 @@ public class SemanticPluginVersion extends PluginVersion{
     
     public SemanticPluginVersion(String versionString) {
         super(versionString);
-        //TODO implementation
+        
+        String[] split_versionString = versionString.split("[\\.-]",4);
+        
+        major = Integer.parseInt(split_versionString[0]);
+        minor =  Integer.parseInt(split_versionString[1]);
+        patch =  Integer.parseInt(split_versionString[2]);
+        identifier = null;
+        buildMetadata = null;
+        
+        if(split_versionString[3].isEmpty())return;
+        int identifier_pos = versionString.indexOf("-");
+        
+        if(identifier_pos == -1){
+        	buildMetadata = split_versionString[3].substring(1, split_versionString[3].length());
+            return;
+        }
+        String sub_identifier = versionString.substring( identifier_pos +1, versionString.length());
+        String[] split_identifier = sub_identifier.split("+",2);
+        if(!split_identifier[0].isEmpty())identifier = split_identifier[0];
+        if(!split_identifier[1].isEmpty())buildMetadata = split_identifier[1];
+        return;
+        
+        
+        
     }
     
     /**
@@ -47,7 +69,33 @@ public class SemanticPluginVersion extends PluginVersion{
     
     @Override
     public boolean isNewerThan(String versionString) {
-        //TODO implementation
-        return false;
+    	SemanticPluginVersion version = new SemanticPluginVersion(versionString);
+    	if(version.major != major)return version.major > major ? true:false;
+    	if(version.minor != minor)return version.minor > minor ? true:false;
+    	if(version.patch != patch)return version.patch > patch ? true:false;
+    	String[] version_1 = identifier.split("\\.") ;
+        String[] version_2 = version.identifier.split("\\.");
+    	int lim =version_1.length > version_2.length ?version_2.length : version_1.length;
+    	if(lim ==0)return version_1.length == 0 ?true : false;
+    	for(int i = 0; i < lim; i++){
+    	    int r  = 0;
+    	    int compare = version_1[i].compareTo(version_2[i]);
+			if(compare != 0) r = compare < 0 ? 1 : -1;
+    		boolean check = false;
+				try {
+    			      int compare1 =Integer.parseInt(version_1[i]);
+    				  check =true;
+    				  int compare2 =Integer.parseInt(version_2[i]);
+    				  if(compare1 != compare2) r = compare1 > compare2 ? 1:-1; 
+    					   
+    				 } catch (NumberFormatException e) {
+    				  r = check == false ? 1 : -1;
+    				 }
+    		   if(r!=0)return r>0?true:false;
+    	  }
+    				
+    	return version_1.length > version_2.length ? false:true;
+    		
+    	
     }
 }
