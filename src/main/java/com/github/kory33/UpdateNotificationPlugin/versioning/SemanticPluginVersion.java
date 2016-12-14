cb6b51b0baeeebf984672c6e68c4baf85739c11a
Kory33 <konryoitdv@gmail.com>
2016-12-14 15:10:22 +0100

    code cleanup

--- src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
+++ src/main/java/com/github/kory33/UpdateNotificationPlugin/versioning/SemanticPluginVersion.java
@@ -26,27 +26,44 @@ public class SemanticPluginVersion extends PluginVersion{
 
      public SemanticPluginVersion(String versionString) {
         super(versionString);
-        if(versionString.matches("^(0|([1-9][0-9]*))\\.(0|([1-9][0-9]*))\\.(0|([1-9][0-9]*))(\\-[0-9A-Za-z\\.]+)?(\\+[0-9A-Za-z\\.]+)?$") == false)throw new IllegalArgumentException();
+        
+        if(versionString.matches("^(0|([1-9][0-9]*))\\.(0|([1-9][0-9]*))\\.(0|([1-9][0-9]*))(\\-[0-9A-Za-z\\.]+)?(\\+[0-9A-Za-z\\.]+)?$") == false){
+            throw new IllegalArgumentException();
+        }
         String[] split_versionString = versionString.split("\\.",3);
         major = Integer.parseInt(split_versionString[0]);
         minor =  Integer.parseInt(split_versionString[1]);
+        
         int identifier_pos = split_versionString[2].indexOf("-");
-        if(split_versionString[2].matches("^[0-9]+$"))patch =  Integer.parseInt(split_versionString[2]);
-        else if(identifier_pos != -1) patch =  Integer.parseInt(split_versionString[2].substring(0,identifier_pos));
-        else if(split_versionString[2].indexOf("+") != -1)patch =  Integer.parseInt( split_versionString[2].substring(0, split_versionString[2].indexOf("+")));
+        if(split_versionString[2].matches("^[0-9]+$")) {
+            patch = Integer.parseInt(split_versionString[2]);
+        }else if(identifier_pos != -1){
+            patch =  Integer.parseInt(split_versionString[2].substring(0,identifier_pos));
+        }else if(split_versionString[2].indexOf("+") != -1){
+            patch =  Integer.parseInt( split_versionString[2].substring(0, split_versionString[2].indexOf("+")));
+        }
+        
         identifier = null;
         buildMetadata = null;
       	String buildMetadata_identifier = null;
-      	if(identifier_pos != -1 || versionString.indexOf("+") != -1)buildMetadata_identifier = versionString.substring((identifier_pos != -1?identifier_pos:versionString.indexOf("+"))+1,versionString.length());
-        if(buildMetadata_identifier==null)return;
+      	if(identifier_pos != -1 || versionString.indexOf("+") != -1){
+      	    buildMetadata_identifier = versionString.substring((identifier_pos != -1?identifier_pos:versionString.indexOf("+"))+1,versionString.length());
+      	}
+        if(buildMetadata_identifier==null){
+            return;
+        }
         if(identifier_pos == -1){
         	buildMetadata =  split_versionString[2].substring(2,  split_versionString[2].length());
             return;
         }
-        String sub_identifier = split_versionString[2].substring( identifier_pos +1, split_versionString[2].length());
+        String sub_identifier = split_versionString[2].substring(identifier_pos +1, split_versionString[2].length());
         String[] split_identifier = sub_identifier.split("\\+",2);
-        if(split_identifier.length >= 1 )identifier = split_identifier[0];
-        if(split_identifier.length >= 2 )buildMetadata = split_identifier[1];
+        if(split_identifier.length >= 1 ) {
+            identifier = split_identifier[0];
+        }
+        if(split_identifier.length >= 2 ) {
+            buildMetadata = split_identifier[1];
+        }
         return;
     }
      
@@ -55,7 +72,7 @@ public class SemanticPluginVersion extends PluginVersion{
      * @return
      */
     public boolean hasIdentifier(){
-        return !this.identifier.equals("");
+        return !"".equals(this.identifier);
     }
     
     /**
@@ -63,34 +80,52 @@ public class SemanticPluginVersion extends PluginVersion{
      * @return true if the version contains any metadata
      */
     public boolean hasBuildMetadata(){
-        return !this.buildMetadata.equals("");
+        return !"".equals(this.buildMetadata);
     }
     
     @Override
     public boolean isNewerThan(String versionString) {
     	SemanticPluginVersion version = new SemanticPluginVersion(versionString);
-    	if(version.major != major)return version.major < major;
-    	if(version.minor != minor)return version.minor < minor;
-    	if(version.patch != patch)return version.patch < patch;
-    	if(version.identifier == null)return false;
-    	if(identifier == null)return version.identifier != null;
+    	if(version.major != major) {
+    	    return version.major < major;
+    	}
+    	if(version.minor != minor) {
+    	    return version.minor < minor;
+    	}
+    	if(version.patch != patch) {
+    	    return version.patch < patch;
+    	}
+    	if(version.identifier == null) {
+    	    return false;
+    	}
+    	if(identifier == null) {
+    	    return version.identifier != null;
+    	}
     	String[] version_1 = identifier.split("\\.") ;
         String[] version_2 = version.identifier.split("\\.");
     	int lim =version_1.length > version_2.length ?version_2.length : version_1.length;
-    	if(lim ==0)return version_1.length == 0;
+    	if(lim ==0) {
+    	    return version_1.length == 0;
+    	}
     	for(int i = 0; i < lim; i++){
     		boolean r = false;
     	    int compare = version_1[i].compareTo(version_2[i]) * -1;
-			if(compare != 0) r = compare < 0;
-			else if(i == lim - 1)return version_1.length > version_2.length;
-			else continue;
-			if (version_1[i].matches("[0-9]+")!=version_2[i].matches("[0-9]+"))return version_1[i].matches("[0-9]+") == false; 
-			else if(version_1[i].matches("[0-9]+") == true){
+			if(compare != 0) {
+			    r = compare < 0;
+			} else if(i == lim - 1) {
+			    return version_1.length > version_2.length;
+			} else {
+			    continue;
+			}
+			if (version_1[i].matches("[0-9]+")!=version_2[i].matches("[0-9]+")) {
+			    return version_1[i].matches("[0-9]+") == false; 
+			} else if(version_1[i].matches("[0-9]+") == true) {
 				int compare1 =Integer.parseInt(version_1[i]);
                 int compare2 =Integer.parseInt(version_2[i]);
     			if(compare1 != compare2) return  compare1 > compare2; 	   
+			} else {
+			    return r;
 			}
-			else return r;
     	}
     	return version.identifier.length() < identifier.length() ;
     }
